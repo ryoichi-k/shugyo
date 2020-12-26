3a7d0e1f888438f47329208525988052d522cc24
加納諒一 <kanouryouichi@kanouryouichinoMacBook-Air.local>
2020-12-26 10:33:50 +0900

    sign-up part1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get  '/signup',  to: 'users#new'
   root 'top_page#home'
+  resources :users
 end
--- test/controllers/top_page_controller_test.rb
+++ test/controllers/top_page_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class TopPageControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get top_page_home_url
+  test "root_path" do
+    get root_path
     assert_response :success
   end
 
