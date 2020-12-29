70b36e54d20a377a948fb3a7eb70d40578f4405d
加納諒一 <kanouryouichi@kanouryouichinoMacBook-Air.local>
2020-12-29 11:22:28 +0900

    implement micropost

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -1,4 +1,8 @@
 class TopPageController < ApplicationController
   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 end
